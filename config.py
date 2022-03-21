05a77d1eceb2d5173422c366c3e17406e3ac2530
cherrysecrets <98997056+cherrysecrets@users.noreply.github.com>
2022-03-21 11:08:03 +0800

    Update config.py

--- config.py
+++ config.py
@@ -5,4 +5,4 @@ BOT_TOKEN = "5219846265:AAHeaXYOyUIlKYBssKbZOpQy6o00W6cF4PQ"
 PROJECT_NAME = "showandtell"
 
 # If you forked the source code, grab your repo's link and paste it below!
-GIT_REPO_URL = "https://github.com/MadeByThePinsHub/GitGram"
+GIT_REPO_URL = "https://github.com/pokurt/GitGram"
